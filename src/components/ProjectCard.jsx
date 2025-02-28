963507df6628755929b889ae7362cb7ed85e8d56
EricRafael-Dev <rafaelpsec@gmail.com>
2025-02-27 22:25:56 -0300

    fetch: rendering projects dynamically [JSON]

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,20 +1,25 @@
 import React from "react";
-import gym_landing_page from "../assets/gym_landing_page.png";
+import projectsData from "../assets/projects.json";
 
 const ProjectCard = () => {
-  return (
-    // Card
-    <div className="bg-[#27163f] h-[650px] w-[500px] rounded-2xl flex flex-col justify-between items-center shadow-2xl shadow-[#27163fa4] inset-shadow-sm inset-shadow-[#451883] transition-transform duration-300 hover:scale-105">
+  const projects = projectsData.projects;
+  const images = import.meta.glob("/src/assets/*.png", { eager: true });
+
+  return projects.map((project, index) => (
+    <div
+      key={project.title}
+      className="bg-[#27163f] h-full min-w-[75vw] rounded-2xl flex flex-col justify-between items-center shadow-2xl shadow-[#27163fa4] inset-shadow-sm inset-shadow-[#451883] transition-transform duration-300 hover:scale-105"
+    >
       {/* Project Img */}
-      <div className="relative h-[50%] overflow-hidden has-hover:[&_div]:flex has-hover:[&_img]:scale-110">
+      <div className="relative overflow-hidden has-hover:[&_div]:flex has-hover:[&_img]:scale-110">
         <img
           className="object-contain rounded-t-3xl transition-transform duration-200"
-          src={gym_landing_page}
+          src={project.image}
           alt=""
         />
-        <div className="absolute top-0 bottom-0 left-0 right-0 bg-gray-700/50 hidden justify-center items-center">
+        <div className="absolute top-0 h-full w-full bg-gray-700/50 hidden rounded-t-3xl justify-center items-center">
           <a
-            href="https://gymlp.vercel.app"
+            href={project.links.deploy}
             target="_blank"
             className="flex justify-center items-center cursor-pointer w-[40%] h-[20%] text-3xl font-bold border-3 border-white rounded-xl hover:bg-white hover:text-gray-700/50 transition-colors duration-200"
           >
@@ -25,16 +30,16 @@ const ProjectCard = () => {
       {/* Description */}
       <div className="flex flex-col px-8 py-5 gap-5 h-auto overflow-auto">
         {/* Title and Category */}
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col text-center justify-between items-center">
           <h1 className="text-3xl font-[AvenirRoman] font-semibold">
-            Página de vendas
+            {project.title}
           </h1>
-          <label className="text-2xl  text-orange-500">Landing-Page</label>
+          <label className="text-2xl  text-orange-500">
+            {project.category}
+          </label>
         </div>
         <p className=" text-[24px] h-auto overflow-auto">
-          Essa landing page foi projetada para promover um kit gratuito que
-          ajuda academias a enfrentar a sazonalidade e manter o faturamento
-          mesmo em períodos de baixa demanda, como inverno e férias.
+          {project.description}
         </p>
       </div>
       {/* Tools Used */}
@@ -42,14 +47,14 @@ const ProjectCard = () => {
       {/* Github / See Deploy */}
       <div className="w-full px-10 py-7 flex justify-between">
         <a
-          href="https://github.com/EricRafael-Dev/landing-page_gym-pt-BR"
+          href={project.links.github}
           target="_blank"
-          className="flex justify-center items-center border-2 border-orange-600 w-[20%] aspect-square rounded-2xl cursor-pointer text-orange-600 hover:text-white hover:bg-orange-600 transition-colors duration-200"
+          className="flex justify-center items-center border-2 border-orange-600 max-w-[25%] aspect-square rounded-2xl cursor-pointer text-orange-600 hover:text-white hover:bg-orange-600 transition-colors duration-200"
         >
           <i className="bx bxl-github bx-lg"></i>
         </a>
         <a
-          href="https://gymlp.vercel.app"
+          href={project.links.deploy}
           target="_blank"
           className="flex justify-center items-center bg-orange-600 w-[70%] rounded-2xl text-2xl font-bold cursor-pointer hover:bg-orange-700 transition-colors duration-200"
         >
@@ -57,7 +62,7 @@ const ProjectCard = () => {
         </a>
       </div>
     </div>
-  );
+  ));
 };
 
 export default ProjectCard;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import ProjectCard from '../components/ProjectCard';
+import { ArrowLeft, ChevronRight } from 'lucide-react';
 
 const Projects = () => {
   return (
-    <section className="w-screen h-screen flex flex-col items-center justify-center gap-15 p-10 ">
-      <div className="relative w-full h-[70%] flex flex-col gap-10 justify-around">
-        <div className="font-[AvenirRoman] font-extrabold text-7xl uppercase text-white/10 ">
+    <section className="w-screen h-screen flex flex-col items-center justify-center gap-15 ">
+      <div className="relative w-full h-[90%] flex flex-col gap-10 justify-around">
+        <div className="font-[AvenirRoman] font-extrabold text-7xl uppercase text-white/10 p-10 ">
           Projetos
         </div>
-        <div className="flex gap-2">
+        <div className="relative flex h-full gap-10 p-10 overflow-x-auto scrollbar-hide">
           <ProjectCard />
+          
         </div>
       </div>
     </section>
